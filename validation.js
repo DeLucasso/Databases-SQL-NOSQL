3068b06009c5819a32e18b35d9c60d812bb0221f
LuCaS <dumdumcz@gmail.com>
2022-06-08 22:58:38 -0700

    Create validation.js

---
+++ validation.js
@@ -0,0 +1,18 @@
+// For validation, you have to have an object in array {}
+
+const fruitSchema = new mongoose.Schema({
+    
+  // Let's REQUIRE the name
+    name: {
+      type: String, required: true,
+    },
+  
+    // Let's set MIN and MAX for the number  
+    rating: {
+    type: Number,
+    min: 1,
+    max: 10 },
+  
+    review: String
+  });
+
